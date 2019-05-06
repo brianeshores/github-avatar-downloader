24f1eb21cca0c00ea78b9a47676002d455ab6a78
Brian Murray <brianmurraysound@gmail.com>
2019-05-06 21:16:02 +0000

    added error message for improper input from node terminal

--- download_avatars.js
+++ download_avatars.js
@@ -3,40 +3,42 @@ var token = require('./secrets.js');
 var fs = require('fs');
 var owner = process.argv[2];
 var name = process.argv[3];
-console.log('owner: ', owner);
-console.log('name: ', name);
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': token
-    }
-  };
+if (owner && name === undefined) {
+  console.log("Repo owner or repo name not specified.  Please try again");
+} else {
+  function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': token
+      }
+    };
 
   request(options, function(err, res, body) {
     cb(err, body);
   });
 }
 
-getRepoContributors(owner, name, function(err, result) {
-  console.log("Errors:", err);
-  // console.log("Result:", result);
-  var parsedData = JSON.parse(result);
-  // console.log('parsed data:', parsedData);
-  parsedData.forEach(function(object){
-    console.log(object.avatar_url);
-    downloadImageByURL(object.avatar_url, 'avatars/' + object.login + '.jpg');
+  getRepoContributors(owner, name, function(err, result) {
+    console.log('owner: ', owner);
+    console.log('name: ', name);
+    console.log("Errors:", err);
+    var parsedData = JSON.parse(result);
+    parsedData.forEach(function(object){
+      console.log(object.avatar_url);
+      downloadImageByURL(object.avatar_url, 'avatars/' + object.login + '.jpg');
+    });
   });
-});
 
-function downloadImageByURL(url, filePath) {
-  request
-  .get(url)
-  .on('error', function(err){
-    console.log('error', error);
-  })
-  .pipe(fs.createWriteStream(filePath));
-  console.log("url: ", url);
-  console.log("file path:", filePath);
-}
+  function downloadImageByURL(url, filePath) {
+    request
+      .get(url)
+      .on('error', function(err){
+        console.log('error', error);
+      })
+      .pipe(fs.createWriteStream(filePath));
+      console.log("url: ", url);
+      console.log("file path:", filePath);
+    }
+  };
