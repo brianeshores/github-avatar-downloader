ca5d82288952dc40f6e42a926b6170a0fc6cc7e4
Brian Murray <brianmurraysound@gmail.com>
2019-05-06 20:39:51 +0000

    added download image by url function. working.

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var token = require('./secrets.js');
+var fs = require('fs');
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -18,8 +19,20 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   // console.log("Result:", result);
   var parsedData = JSON.parse(result);
-  console.log('parsed data:', parsedData);
+  // console.log('parsed data:', parsedData);
   parsedData.forEach(function(object){
     console.log(object.avatar_url);
-  })
+    downloadImageByURL(object.avatar_url, 'avatars/' + object.login + '.jpg');
+  });
 });
+
+function downloadImageByURL(url, filePath) {
+  request
+  .get(url)
+  .on('error', function(err){
+    console.log('error', error);
+  })
+  .pipe(fs.createWriteStream(filePath));
+  console.log("url: ", url);
+  console.log("file path:", filePath);
+}
